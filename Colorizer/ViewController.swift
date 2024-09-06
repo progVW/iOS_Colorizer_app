5546118421e8f20432b92f762674f90bf6356891
Sergey Sukharev <suharev.s@icloud.com>
2024-09-06 18:40:46 +0300

    Added red, blue and green button with functionals.

--- Colorizer/ViewController.swift
+++ Colorizer/ViewController.swift
@@ -33,23 +33,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func redButtonAction(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
         count = 5
+        printTimeValue()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countdownTimer), userInfo: nil, repeats: true)
         backgroundView.backgroundColor = UIColor.red
+        
     }
     
     @objc func countdownTimer() {
+        print("redButton pressed, passed time: \(count).")
         count -= 1
         if count == 0 {
             timer?.invalidate()
             timer = nil
+            backgroundView.backgroundColor = UIColor.darkGray
         }
+        printTimeValue()
     }
     
     @IBAction func greenButtonAction(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
+        count = 5
+        printTimeValue()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countdownTimer), userInfo: nil, repeats: true)
+        backgroundView.backgroundColor = UIColor.green
     }
     
     @IBAction func blueButtonAction(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
+        count = 5
+        printTimeValue()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countdownTimer), userInfo: nil, repeats: true)
+        backgroundView.backgroundColor = UIColor.blue
     }
     
 }
